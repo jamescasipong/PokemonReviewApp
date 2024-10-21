fe67a9a55ad46ae8b02f72b9239c652b748f5f3c
James Casipong <144509235+jamescasipong@users.noreply.github.com>
2024-10-21 19:43:52 +0800

    Initial commit

---
+++ Models/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public ICollection<Review> ReaderLocks { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> MyProperty {  get; set; }
+
+    }
+}
